df0c2a777229a4709475168036ab4307195b1072
MykhailoVobolis <145288987+MykhailoVobolis@users.noreply.github.com>
2024-02-11 08:08:38 +0000

    Deploying to gh-pages from @ MykhailoVobolis/goit-js-hw-10@73f313d9459f2383fbf98966e9d6cddda7b8dfaa 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{f as n}from"./assets/vendor-2b44ac2e.js";const o=document.querySelector("#datetime-picker");o.addEventListener("input",i);const t={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}};function i(e){n("#datetime-picker",t),console.log(t.defaultDate)}
+//# sourceMappingURL=commonHelpers.js.map
